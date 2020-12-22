32caed47df2950a2215a87fbc089a4752aca135f
MohammedThowfiq <mohammedthowfiq2@gmail.com>
2020-12-22 17:35:12 +0530

    First Commit

---
+++ lib/colors.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+mixin Coolors {
+  static const Color primaryColor = Color(0xff252733);
+  static const Color secondaryColor = Colors.black;
+  // static const Color accentColor = Color(0xff813362);
+  static Color accentColor = Vx.purple400;
+  static List<Color> colors = [
+    Vx.green400,
+    Vx.yellow400,
+    Vx.blue400,
+    Vx.red400,
+    Vx.teal400,
+    Vx.orange400,
+    Vx.pink400,
+    Vx.purple400,
+  ];
+}
---
+++ lib/header.dart
@@ -0,0 +1,174 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:portfolio_x/colors.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HeaderScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final nameWidget = "Mohammed\nThowfiq"
+        .text
+        .white
+        .xl4
+        .lineHeight(1)
+        .size(context.isMobile ? 15 : 20)
+        .bold
+        .make()
+        .shimmer();
+    return SafeArea(
+      child: VxBox(
+              child: VStack([
+        ZStack(
+          [
+            PictureWidget(),
+            Row(
+              children: [
+                VStack([
+                  if (context.isMobile) 50.heightBox else 10.heightBox,
+                  CustomAppBar().shimmer(primaryColor: Coolors.accentColor),
+                  30.heightBox,
+                  nameWidget,
+                  20.heightBox,
+                  VxBox()
+                      .color(Coolors.accentColor)
+                      .size(60, 10)
+                      .make()
+                      .px4()
+                      .shimmer(primaryColor: Coolors.accentColor),
+                  30.heightBox,
+                  SocialAccounts(),
+                ]).pSymmetric(
+                  h: context.percentWidth * 10,
+                  v: 32,
+                ),
+                Expanded(
+                  child: VxResponsive(
+                    medium: IntroductionWidget()
+                        .pOnly(left: 120)
+                        .h(context.percentHeight * 60),
+                    large: IntroductionWidget()
+                        .pOnly(left: 120)
+                        .h(context.percentHeight * 60),
+                    fallback: const Offstage(),
+                  ),
+                )
+              ],
+            ).w(context.screenWidth)
+          ],
+        )
+      ]))
+          .size(context.screenWidth, context.percentHeight * 60)
+          .color(Coolors.secondaryColor)
+          .make(),
+    );
+  }
+}
+
+class IntroductionWidget extends StatelessWidget {
+  const IntroductionWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VStack(
+      [
+        [
+          " - About".text.gray500.widest.sm.make(),
+          10.heightBox,
+          "Hey, this is Mohammed Thowfiq, I'm a final year IT undergraduate. I love developing front-end applications and devising creative solutions to intricate problems. I'm a curious learner and a passion driven person about the things I do. I give my 100% even in getting the minute fragments of the whole process right. Apart from this, I am enthralled to watching movies and cricket. So pick a game from history or suggest me a movie, that should do for an interesting conversation!"
+              .text
+              .white
+              .xl2
+              .center
+              .maxLines(20)
+              .make()
+              .w(context.isMobile
+                  ? context.screenWidth
+                  : context.percentWidth * 40),
+          20.heightBox,
+        ].vStack(),
+        RaisedButton(
+          onPressed: () {
+            launch("https://www.linkedin.com/in/mohammedthowfiq/");
+          },
+          hoverColor: Vx.purple700,
+          shape: Vx.roundedSm,
+          color: Coolors.accentColor,
+          textColor: Coolors.primaryColor,
+          child: "Visit mohammedthowfiq.com".text.make(),
+        ).h(50)
+      ],
+      // crossAlignment: CrossAxisAlignment.center,
+      alignment: MainAxisAlignment.spaceEvenly,
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      AntDesign.codesquare,
+      size: 50,
+      color: Coolors.accentColor,
+    );
+  }
+}
+
+class PictureWidget extends StatelessWidget {
+  const PictureWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform(
+      alignment: Alignment.center,
+      origin: Offset(context.percentWidth * 2, 0),
+      transform: Matrix4.rotationY(pi * 2),
+      child: Center(
+        child: Image.asset(
+          "assets/new_pic.jpeg",
+          fit: BoxFit.cover,
+          height: context.percentHeight * 60,
+          width: context.percentWidth * 100,
+        ),
+      ),
+    );
+  }
+}
+
+class SocialAccounts extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return [
+      Icon(
+        AntDesign.linkedin_square,
+        color: Colors.white,
+      ).mdClick(() {
+        launch("https://www.linkedin.com/in/mohammedthowfiq/");
+      }).make(),
+      20.widthBox,
+      Icon(
+        AntDesign.instagram,
+        color: Colors.white,
+      ).mdClick(() {
+        launch("https://www.instagram.com/mohammed.thowfiq17/");
+      }).make(),
+      20.widthBox,
+      Icon(
+        AntDesign.github,
+        color: Colors.white,
+      ).mdClick(() {
+        launch("https://github.com/MohammedThowfiq/");
+      }).make()
+    ].hStack();
+  }
+}
